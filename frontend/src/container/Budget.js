4204aa07233502e8d1103fb3ddb8498cae15e5af
Tiffc4484 <tiffanyc4484@gmail.com>
2021-04-07 19:42:45 -0700

    style of progess bar, add description (left/over)

--- frontend/src/container/Budget.js
+++ frontend/src/container/Budget.js
@@ -5,30 +5,39 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 export default function Budget(props) {
   function setBudget() {}
   const currentExpenses = props.expense;
-  const budget = 1000;
+  const budget = 500;
   const barData = [];
+  // console.log(currentExpenses);
   Object.keys(currentExpenses).map((item) => {
+    let totalExpense = 0;
     currentExpenses[item].map((item) => {
-      const ratio = (item.amount / budget) * 100;
-      const obj = {
-        amount: item.amount,
-        ratio: ratio,
-        budget: budget,
-      };
-      barData.push(obj);
+      totalExpense += item.amount;
     });
+
+    const object = {};
+    const ratio = ((totalExpense / budget) * 100).toFixed(2);
+    object["amount"] = totalExpense;
+    object["ratio"] = ratio;
+    object["budget"] = budget;
+    object["category"] = item;
+    object["left"] = budget - totalExpense;
+    barData.push(object);
   });
 
   function getVariant(ratio) {
     if (ratio < 25) {
-      return "success";
-    }
-
-    if (ratio < 50) {
       return "info";
     }
 
+    if (ratio < 50) {
+      return "success";
+    }
+
     if (ratio < 75) {
+      return "";
+    }
+
+    if (ratio < 100) {
       return "warning";
     }
 
@@ -40,17 +49,30 @@ export default function Budget(props) {
   });
 
   return (
-    <div className="flex-container">
+    <div
+      className="flex-container"
+      style={{ height: "80vh", overflowY: "scroll" }}
+    >
       <div className="ProgessBar flex-column align-items-center .ml-1">
-        {barData.forEach((item) => {
+        {barData.map((item) => {
           return (
-            <div>
+            <div
+              key={item.category}
+              style={{ padding: "5px", width: "70%", margin: "0 auto" }}
+            >
+              {item.category}
               <ProgressBar
-                now={item.amount}
+                now={item.amount.toFixed(2)}
                 variant={getVariant(item.ratio)}
-                label={`${(item.amount / budget).toFixed(2) * 100}%`}
+                label={`${item.ratio}%`}
                 max={budget}
               />
+              {`$${item.amount.toFixed(2)} of $${budget}`}
+              <span style={{ float: "right" }}>
+                {item.left > 0
+                  ? `${item.left.toFixed(2)} left`
+                  : `${Math.abs(item.left).toFixed(2)} Over`}
+              </span>
             </div>
           );
         })}
