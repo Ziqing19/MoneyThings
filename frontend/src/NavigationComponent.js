2e131f5c95e4e0ee1ed64e011140c2b5327d345c
Ziqing19 <mzq5176@gmail.com>
2021-04-13 16:41:17 -0700

    fix deployment

--- frontend/src/NavigationComponent.js
+++ frontend/src/NavigationComponent.js
@@ -12,7 +12,7 @@ export default function NavigationComponent(props) {
       alert(res);
     }
     document.cookie = "_id=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    window.location = "/auth/login";
+    window.location = "/";
   }
 
   useEffect(() => {
